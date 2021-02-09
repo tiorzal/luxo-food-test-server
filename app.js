37eb78bcaa73b644eacd7ccd3c7e9eb8494ab2bb
tiorzal <tioralmunazir@gmail.com>
2021-02-09 07:38:06 +0700

    forgot to save

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//env set up for jwt secret
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 const { ApolloServer } = require("apollo-server");
+
+//import typedef & resolver
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-
+//new server instance
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,6 +18,7 @@ const server = new ApolloServer({
   }
 })
 
+//server start
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
